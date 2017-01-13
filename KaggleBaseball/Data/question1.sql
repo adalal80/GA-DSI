f7d45d759600d0d6561535327952d380bb15391a
Amish Dalal <amish.dalal@gmail.com>
2017-01-12 22:38:29 -0600

    kagglebaseball

--- KaggleBaseball/Data/question1.sql
+++
@@ -1,9 +0,0 @@
-Select b.player_id, b.year, b.team_id, b.rbi, b.bb, a.g_all, a.g_3b,s.salary from batting as b
-join appearances as a
-on b.player_id = a.player_id
-and b.year = a.year
-and b.team_id = a.team_id
-join salary as s
-on b.player_id = s.player_id
-and b.year = s.year
-and b.team_id = s.team_id
